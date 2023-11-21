a2f96b8b475a6a9f62acf118ba930e97764090ba
Ulf Adams <ulf@engflow.com>
2023-11-21 22:21:46 +0100

    Backport a grpc-java leak fix.
    https://github.com/grpc/grpc-java/commit/5a2c94bca1eed13d74fe13ce0d0b534b6e4b26a5

--- core/src/main/java/io/grpc/internal/RetriableStream.java
+++ core/src/main/java/io/grpc/internal/RetriableStream.java
@@ -1056,6 +1056,7 @@ abstract class RetriableStream<ReqT> implements ClientStream {
       checkState(
           savedState.winningSubstream != null, "Headers should be received prior to messages.");
       if (savedState.winningSubstream != substream) {
+        GrpcUtil.closeQuietly(producer);
         return;
       }
       listenerSerializeExecutor.execute(
